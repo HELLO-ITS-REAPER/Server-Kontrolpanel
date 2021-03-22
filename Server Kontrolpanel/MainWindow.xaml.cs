fd328293a66927724297aaafb8941a2d66b2a297
Rezan Rasoul <t-xe@hotmail.com>
2021-03-22 12:48:09 +0100

    add  more info  buttins  to  servers

--- Server Kontrolpanel/MainWindow.xaml.cs
+++ Server Kontrolpanel/MainWindow.xaml.cs
@@ -21,39 +21,51 @@ namespace Server_Kontrolpanel
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        SshClient client1 = new SshClient("95.179.187.146", "root", "$s8PtAa)uEZW{H[2");
+        SshClient client2 = new SshClient("45.76.43.221", "root", "j}7E(Ma38tMg8_ux");
+        SshClient client3 = new SshClient("45.32.232.37", "root", "!Tz53@_BJ(RCdXk@");
         public MainWindow()
         {
             InitializeComponent();
 
-            SshClient client1 = new SshClient("95.179.187.146", "root", "$s8PtAa)uEZW{H[2");
-            SshClient client2 = new SshClient("45.76.43.221", "root", "j}7E(Ma38tMg8_ux");
-            SshClient client3 = new SshClient("45.32.232.37", "root", "!Tz53@_BJ(RCdXk@");
+            
+
             client1.Connect();
             client2.Connect();
             client3.Connect();
+            try
+            {
+                if (client1.IsConnected)
+                {
+                    Status1.Fill = Brushes.Green;
+                }
+                if (client2.IsConnected)
+                {
+                    Status2.Fill = Brushes.Green;
+                }
+                if (client3.IsConnected)
+                {
+                    Status3.Fill = Brushes.Green;
+                }
+            }
+            catch (Exception)
+            {
 
-            if (client1.IsConnected)
-            {
-                Status1.Fill = Brushes.Green;
-            }
-            if (client2.IsConnected)
-            {
-                Status2.Fill = Brushes.Green;
-            }
-            if (client3.IsConnected)
-            {
-                Status3.Fill = Brushes.Green;
+                Status3.Fill = Brushes.Gray;
             }
         }
 
         private void Server1Reboot_Click(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         private void Server1Info_Click(object sender, RoutedEventArgs e)
         {
-
+            Window1 rr = new Window1();
+            rr.Show();
+            rr.info1();
         }
 
         private void Server2Reboot_Click(object sender, RoutedEventArgs e)
@@ -64,6 +76,9 @@ namespace Server_Kontrolpanel
         private void Server2Info_Click(object sender, RoutedEventArgs e)
         {
 
+            Window1 rr = new Window1();
+            rr.Show();
+            rr.info3();
         }
 
         private void Server3Reboot_Click(object sender, RoutedEventArgs e)
@@ -74,6 +89,9 @@ namespace Server_Kontrolpanel
         private void Server3Info_Click(object sender, RoutedEventArgs e)
         {
 
+            Window1 rr = new Window1();
+            rr.Show();
+            rr.info3();
         }
 
         private void Edit1_Click(object sender, RoutedEventArgs e)
---
+++ Server Kontrolpanel/Window1.xaml.cs
@@ -0,0 +1,169 @@
+﻿using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Server_Kontrolpanel
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+
+        SshClient client2 = new SshClient("45.76.43.221", "root", "j}7E(Ma38tMg8_ux");
+        SshClient client3 = new SshClient("45.32.232.37", "root", "!Tz53@_BJ(RCdXk@");
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        public void info1()
+        {
+            using (var client = new SshClient("95.179.187.146",22, "root", "$s8PtAa)uEZW{H[2"))
+            {
+                try
+                {
+                    client.Connect();
+                    if (client.IsConnected)
+                    {
+                        SshCommand sc = client.CreateCommand("ps -f");
+                        SshCommand rr = client.CreateCommand("hostname");
+                        SshCommand upTime = client.CreateCommand("uptime -p");
+                        SshCommand ram = client.CreateCommand("echo $(($(getconf _PHYS_PAGES) * $(getconf PAGE_SIZE) / (1024 * 1024)))");
+                        SshCommand disk = client.CreateCommand("df -h | grep '/dev/vda1' | awk '{print $3}' | sed 's/G/ GB/'");
+                        SshCommand cpu = client.CreateCommand("top -b -n 2 -d 0.2 | tail -1 | awk '{print $9}'");
+
+
+
+                        sc.Execute();
+                        rr.Execute();
+                        upTime.Execute();
+                        ram.Execute();
+                        disk.Execute();
+                        cpu.Execute();
+                        servername.Content = rr.Result;
+                        uptime.Content = upTime.Result;
+                        Cpu.Content = cpu.Result;
+                        
+                        Ram.Content = ram.Result;
+                        Disk.Content = disk.Result;
+                        process.Items.Add(sc.Result);
+
+                    }
+                }
+                catch (Exception)
+                {
+
+                    servername.Content = "false";
+                }
+
+            }
+            
+
+        }
+
+
+        public void info2()
+        {
+            using (var client = new SshClient("45.76.43.221", 22, "root", "j}7E(Ma38tMg8_ux"))
+            {
+                try
+                {
+                    client.Connect();
+                    if (client.IsConnected)
+                    {
+                        SshCommand sc = client.CreateCommand("ps -f");
+                        SshCommand rr = client.CreateCommand("hostname");
+                        SshCommand upTime = client.CreateCommand("uptime -p");
+                        SshCommand ram = client.CreateCommand("echo $(($(getconf _PHYS_PAGES) * $(getconf PAGE_SIZE) / (1024 * 1024)))");
+                        SshCommand disk = client.CreateCommand("df -h | grep '/dev/vda1' | awk '{print $3}' | sed 's/G/ GB/'");
+                        SshCommand cpu = client.CreateCommand("top -b -n 2 -d 0.2 | tail -1 | awk '{print $9}'");
+
+
+
+                        sc.Execute();
+                        rr.Execute();
+                        upTime.Execute();
+                        ram.Execute();
+                        disk.Execute();
+                        cpu.Execute();
+                        servername.Content = rr.Result;
+                        uptime.Content = upTime.Result;
+                        Cpu.Content = cpu.Result;
+
+                        Ram.Content = ram.Result;
+                        Disk.Content = disk.Result;
+                        process.Items.Add(sc.Result);
+
+                    }
+                }
+                catch (Exception)
+                {
+
+                    servername.Content = "false";
+                }
+
+            }
+
+
+        }
+
+        public void info3()
+        {
+            using (var client = new SshClient("45.32.232.37", 22, "root", "!Tz53@_BJ(RCdXk@"))
+            {
+                try
+                {
+                    client.Connect();
+                    if (client.IsConnected)
+                    {
+                        SshCommand sc = client.CreateCommand("ps -f");
+                        SshCommand rr = client.CreateCommand("hostname");
+                        SshCommand upTime = client.CreateCommand("uptime -p");
+                        SshCommand ram = client.CreateCommand("echo $(($(getconf _PHYS_PAGES) * $(getconf PAGE_SIZE) / (1024 * 1024)))");
+                        SshCommand disk = client.CreateCommand("df -h | grep '/dev/vda1' | awk '{print $3}' | sed 's/G/ GB/'");
+                        SshCommand cpu = client.CreateCommand("top -b -n 2 -d 0.2 | tail -1 | awk '{print $9}'");
+
+
+
+                        sc.Execute();
+                        rr.Execute();
+                        upTime.Execute();
+                        ram.Execute();
+                        disk.Execute();
+                        cpu.Execute();
+                        servername.Content = rr.Result;
+                        uptime.Content = upTime.Result;
+                        Cpu.Content = cpu.Result;
+
+                        Ram.Content = ram.Result;
+                        Disk.Content = disk.Result;
+                        process.Items.Add(sc.Result);
+
+                    }
+                }
+                catch (Exception)
+                {
+
+                    servername.Content = "false";
+                }
+
+            }
+
+
+        }
+
+    }
+
+}
